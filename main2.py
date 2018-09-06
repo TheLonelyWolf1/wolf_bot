8378044b8b43b3104f21f86d3ec59ff53c4270ce
TheLonelyWolf1 <42873246+TheLonelyWolf1@users.noreply.github.com>
2018-09-06 16:57:50 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -558,5 +558,5 @@ async def info(ctx,):
     embed.add_field(name="Owner:", value="TheLonelyWolf")
     embed.add_field(name="Gründungsdatum:", value="__***Die 218.Gaming Division wurde am 20. Juni 2018 von TheLonelyWolf gegründet!***__")
     await bot.say(embed=embed)
-token = os.environ.get("TOKEN")
+token = os.environ.get("TOKEN2")
 bot.run(token)

5deb9a5728d378fb8b7b5df467d76118858bbb14
TheLonelyWolf1 <42873246+TheLonelyWolf1@users.noreply.github.com>
2018-09-09 14:53:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -85,12 +85,7 @@ async def on_ready():
 
 
 if not discord.opus.is_loaded():
-    # the 'opus' library here is opus.dll on windows
-    # or libopus.so on linux in the current directory
-    # you should replace this with the location the
-    # opus library is located in and with the proper filename.
-    # note that on windows this DLL is automatically provided for you
-discord.opus.load_opus('opus')
+   discord.opus.load_opus('opus')
 
 class VoiceEntry:
     def __init__(self, message, player):
